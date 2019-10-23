773796c4dae33eba46b84d722ecd2ac2599a2c1a
Tijs Teulings <tijs.t@hotmail.com>
2019-10-23 13:37:54 +0200

    added mock logboek

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ from google_calendar import list_events, create_event
 
 import json
 
+import os, random
+
 app = Flask(__name__)
 
 app.config['BASIC_AUTH_USERNAME'] = 'tet'
@@ -36,7 +38,9 @@ def start():
         order_items = collections.OrderedDict(reversed(list(sl_items.items())))
 
 
-    return render_template("index.html", sl_items=order_items)
+    background_img = random.choice(os.listdir("./static/img"))
+    print(background_img)
+    return render_template("index.html", sl_items=order_items, background_img=background_img)
 
 @app.route('/calendar', methods=["GET", "POST"])
 def calendar():
@@ -107,84 +111,6 @@ def remove_from_list():
         json.dump(sl_items, output_file)
 
     return jsonify("")
-
-# @app.route('/agenda')
-# def agenda():
-
-#     return render_template("agenda.html")
-
-
-
-# @app.route('/logboek', methods=["GET", "POST"])
-# def logboek():
-
-#     # if request.method == "POST":
-#     #     return render_template("new_entry.html")
-
-
-
-#     return render_template("logboek.html", messages=messages)
-
-# @app.route('/new_entry', methods=["GET", "POST"])
-# def new_entry():
-
-#     if request.method == "POST":
-#         with sqlite3.connect("logboek.db") as con:
-#             cur = con.cursor()
-
-#             start = request.form["start"]
-#             end = request.form["end"]
-#             message = request.form["entry"]
-
-#             if not (start or end or message):
-#                 return render_template("new_entry.html")
-
-#             cur.execute("insert into logboekv2(start, end, user, message) values (?,?,?,?)", (start, end, "admin", message))
-
-#         return redirect(url_for("logboek"))
-
-#     return render_template("new_entry.html")
-
-
-# @app.route('/to_do', methods=["GET", "POST"])
-# def to_do():
-#     if request.method == "POST":
-#         if request.form["add_btn"] == "add":
-#             print("test")
-
-#             add_to_do()
-#         elif request.form["rmv_btn"] == "rmv":
-#             print("test")
-
-
-#     with sqlite3.connect("to_do.db") as con:
-#         cur = con.cursor()
-#         cur.execute("select * from to_do")
-
-#         fetchs  = cur.fetchall()
-
-#         items = []
-#         for fetch in fetchs:
-#             item = {}
-
-#             item["date"] = timeago.format(str(fetch[1][:-7]))
-#             item["user"] = str(fetch[2])
-#             item["item"] = str(fetch[3])
-
-#             items.insert(0, item)
-
-
-#     return render_template("to_do.html", to_dos=items)
-
-# def add_to_do():
-#     with sqlite3.connect("to_do.db") as con:
-#             cur = con.cursor()
-
-#             item = request.form["item"]
-
-#             cur.execute("insert into to_do(date, user, item) values (?,?,?)", (str(datetime.datetime.now()), "admin", item))
-
-#     return redirect(url_for("to_do"))
 
 if __name__ == '__main__':
     app.run(debug=True)
