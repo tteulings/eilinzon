1e504ffdb7a5b6fd4a9821af789970f8ec67a04c
Tijs Teulings <tijs.t@hotmail.com>
2019-10-29 21:25:19 +0100

    added logboek

--- app.py
+++ app.py
@@ -23,22 +23,23 @@ username="Familie"
 @app.route('/',methods=["GET", "POST"])
 def start():
     if request.method == "POST":
-        print(request.form)
         description = request.form["description"]
-        start = str(request.form["start"])
-        end = str(request.form["end"])
+        start, end = str(request.form["daterange"]).split(" tot ")
 
-        print(start)
+        create_event.create_event(start, end , description)
 
-        create_event.create_event(start,end,description)
-
-    with open("logboek.json") as json_file:
+    print("tet")
+    with open("boodschappenlijst.json") as json_file:
         sl_items = json.load(json_file)
         order_items = collections.OrderedDict(reversed(list(sl_items.items())))
 
+    with open("logboek.json") as json_file:
+        log_items = json.load(json_file)
+        log_items = collections.OrderedDict(reversed(list(log_items.items())))
+
 
     background_img = random.choice(os.listdir("./static/img"))
-    return render_template("index.html", sl_items=order_items, background_img=background_img)
+    return render_template("index.html", sl_items=order_items, log_items=log_items, background_img=background_img)
 
 @app.route('/calendar', methods=["GET", "POST"])
 def calendar():
@@ -82,30 +83,55 @@ def shopping_list():
 @app.route('/add', methods=["GET", "POST"])
 def add_to_list():
 
-    with open("logboek.json", mode="r") as json_file:
+    with open("boodschappenlijst.json", mode="r") as json_file:
         sl_items = json.load(json_file)
+
+    date_cur = datetime.strftime(date.today(), '%d-%m-%Y')
 
     new_item = {request.args.get('item') : {
         "user" : request.args.get('user'),
-        "datetime" : "{}".format(date.today()),
+        "datetime" : date_cur,
         "quantity" : request.args.get('quantity')
     }}
 
     sl_items.update(new_item)
 
-    with open("logboek.json", mode="w") as output_file:
+    with open("boodschappenlijst.json", mode="w") as output_file:
         json.dump(sl_items, output_file)
+
+    return jsonify(new_item)
+
+@app.route('/add_log', methods=["GET", "POST"])
+def add_log():
+
+    with open("logboek.json", mode="r") as json_file:
+        log_items = json.load(json_file)
+
+    key = int(list(log_items.keys())[-1]) + 1
+    print(request.args.get('datetime'))
+
+    new_item = {key : {
+        "author" : request.args.get('author'),
+        "datetime" : request.args.get('datetime'),
+        "description" : request.args.get('description')
+    }}
+
+
+    log_items.update(new_item)
+
+    with open("logboek.json", mode="w") as output_file:
+        json.dump(log_items, output_file)
 
     return jsonify(new_item)
 
 @app.route('/remove', methods=["GET", "POST"])
 def remove_from_list():
-    with open("logboek.json", mode="r") as json_file:
+    with open("boodschappenlijst.json", mode="r") as json_file:
         sl_items = json.load(json_file)
 
     sl_items.pop(request.args.get('item'))
 
-    with open("logboek.json", mode="w") as output_file:
+    with open("boodschappenlijst.json", mode="w") as output_file:
         json.dump(sl_items, output_file)
 
     return jsonify("")
--- google_calendar/create_event.py
+++ google_calendar/create_event.py
@@ -3,27 +3,21 @@ from google_calendar.cal_setup import get_calendar_service
 
 
 def create_event(start, end, description):
-       # creates one hour event tomorrow 10 AM IST
-
     service = get_calendar_service()
 
-    start = datetime.strptime(start, "%Y-%m-%d").isoformat()
-    end = datetime.strptime(end, "%Y-%m-%d").isoformat()
+    start = datetime.strptime(start, "%d-%m-%Y")
+    end = datetime.strptime(end, "%d-%m-%Y")
+    end = (end + timedelta(hours=23, minutes=59))
 
-    # tomorrow = datetime(d.year, d.month, d.day, 10)+timedelta(days=1)
-    # start = tomorrow.isoformat()
-    # end = (tomorrow + timedelta(hours=1)).isoformat()
 
     event_result = service.events().insert(calendarId='eilinzon@gmail.com',
                                            body={
                                                "summary": f"REQUEST: {description}",
                                                "description": 'Made by website',
-                                               "start": {"dateTime": start, "timeZone": 'Europe/Amsterdam'},
-                                               "end": {"dateTime": end, "timeZone": 'Europe/Amsterdam'},
+                                               "start": {"dateTime": start.isoformat(), "timeZone": 'Europe/Amsterdam'},
+                                               "end": {"dateTime": end.isoformat(), "timeZone": 'Europe/Amsterdam'},
                                            }
                                            ).execute()
-
-    print(event_result)
 
 if __name__ == '__main__':
     create_event(0,0,"Test")
